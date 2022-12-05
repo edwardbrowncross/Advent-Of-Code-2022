22f8725fac84fac33c87ebb0b4caf3a9473c72e0
Edward Browncross <e.browncross@sheffield.ac.uk>
2022-12-05 10:23:15 +0000

    day 5

--- scripts/run.js
+++ scripts/run.js
@@ -12,7 +12,7 @@ const AOC_URL = `https://adventofcode.com/${AOC_YEAR}/day/${AOC_DAY}`;
 // May I be forgiven for this lazy use of global variables and the spaghetti code it produces 🙏
 let example, solution1, solution2, done1, done2;
 let input;
-const incorrectAnswers = [[],[]]
+const incorrectAnswers = [null, [],[]]
 
 const loadWebData = async () => {
   const response = await fetch(`${AOC_URL}`, {
@@ -75,15 +75,17 @@ const submitAnswer = async (answer, part) => {
   }
 }
 
+const clone = obj => JSON.parse(JSON.stringify(obj));
+
 const checkExample = (input, expected, fn) => {
-  const result = fn(input);
+  const result = fn(clone(input));
   const ok = String(result) === expected;
   console.log(`Example: \t${result} \t${ok ? "✅" : `❌ (expected ${expected})`}`);
   return ok;
 }
 
-const runRealInput = async (input, done, fn) => {
-  const result = fn(input);
+const runRealInput = (input, done, fn) => {
+  const result = fn(clone(input));
   console.log(`Result: \t${result} \t${done ? "✅" : "❌"}`);
   return result;
 }
@@ -107,7 +109,7 @@ const onCodeChange = async () => {
   console.log('--------- Part 1 ---------');
   try {
     // Check whether part1 actually returns something
-    if (code.part1(parsedExample) === undefined) {
+    if (code.part1(clone(parsedExample)) === undefined) {
       console.log(JSON.stringify(parsedExample));
       console.log("Part 1 returning undefined");
       return;
--- src/5.js
+++ src/5.js
@@ -1,25 +1,44 @@
 import * as p from '../utils/parser.js';
 import * as u from '../utils/utils.js';
 
-export const parseInput = p.chain(p.blocks([
-  p.chain(p.grid(), grid => {
-    return u.transpose(
-      grid
-        .slice(0, -1)
-        .map((row) => row.filter((_, i) => i % 4 === 1))
-    );
-  }),
-  p.lines(p.chain(p.words(p.number), x => x.filter(y => typeof y === 'number'), p.name(['count', 'from', 'to']))),
-]), p.name(['stacks', 'moves']));
+// So much for my neat and concise parsing utils
+export const parseInput = p.chain(
+  p.blocks([
+    p.chain(
+      p.grid(),
+      grid => {
+        return u.transpose(
+          grid
+            .slice(0, -1)
+            .map((row) =>
+              row.filter((_, i) => i % 4 === 1)
+            )
+        ).map(row => row.filter(x => x !== ' '))
+      }),
+    p.lines(p.chain(
+      p.words(p.number),
+      x => x.filter(y => typeof y === 'number'),
+      p.name(['count', 'from', 'to'])
+    )),
+  ]),
+  p.name(['stacks', 'moves'])
+);
+
+const move = reverse => (stacks, { from, to, count }) => {
+  const items = stacks[from - 1].splice(0, count);
+  if (reverse) {
+    items.reverse();
+  }
+  stacks[to - 1].unshift(...items);
+  return stacks;
+}
 
 export const part1 = (input) => {
-
-
-  return;
+  const stacks = input.moves.reduce(move(true), input.stacks);
+  return stacks.map(s => s[0]).join('');
 }
 
 export const part2 = (input) => {
-
-
-  return;
+  const stacks = input.moves.reduce(move(false), input.stacks);
+  return stacks.map(s => s[0]).join('');
 }
